61c1591b3176db8a76de51f9529b2ee380eee3ff
Ted Chen <imaging8896@gmail.com>
2025-10-11 13:16:38 +0800

    chore: Fix typing

--- news/__init__.py
+++ news/__init__.py
@@ -1,7 +1,6 @@
 import logging
 import random
 
-
 from cloudscraper import CloudScraper
 from curl_cffi import requests as curl_requests
 
@@ -20,12 +19,12 @@ def get_news_content(url: str, mobile: bool = True, desktop: bool = True):
     return parser.content, news_url
 
 
-def get_news_raw_content(url: str, mobile: bool = True, desktop: bool = True):
+def get_news_raw_content(url: str, mobile: bool = True, desktop: bool = True, **request_kw):
     response = None
     exception = None
     
     try:
-        response = get_news_raw_content_by_cloudscraper(url, mobile=mobile, desktop=desktop)
+        response = get_news_raw_content_by_cloudscraper(url, mobile=mobile, desktop=desktop, **request_kw)
     except (curl_requests.exceptions.SSLError, curl_requests.exceptions.ConnectionError) as e:
         logger.warning(f"Request error with random client header", exc_info=True)
         exception = e
@@ -33,14 +32,14 @@ def get_news_raw_content(url: str, mobile: bool = True, desktop: bool = True):
     if mobile and desktop:
         try:
             if response is None or response.status_code == 403:
-                response = get_news_raw_content_by_cloudscraper(url, mobile=False)
+                response = get_news_raw_content_by_cloudscraper(url, mobile=False, **request_kw)
         except (curl_requests.exceptions.SSLError, curl_requests.exceptions.ConnectionError) as e:
             logger.warning(f"Request error with desktop client header", exc_info=True)
             exception = e
 
         try:
             if response is None or response.status_code == 403:
-                response = get_news_raw_content_by_cloudscraper(url, desktop=False)
+                response = get_news_raw_content_by_cloudscraper(url, desktop=False, **request_kw)
         except (curl_requests.exceptions.SSLError, curl_requests.exceptions.ConnectionError) as e:
             logger.warning(f"Request error with mobile client header", exc_info=True)
             exception = e
